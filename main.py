dc10ca0501605ca070df4d7fca5d0c5d05cef5e8
PranavByali <63644120+PranavByali@users.noreply.github.com>
2021-04-19 19:05:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ data = [{"Gender": "Male", "HeightCm": 171, "WeightKg": 96},
         {"Gender": "Female", "HeightCm": 150, "WeightKg": 70},
         {"Gender": "Female", "HeightCm": 167, "WeightKg": 82}]
 
+def test_get_bmi():
+    assert get_bmi([{"Gender": "Male", "HeightCm": 171, "WeightKg": 96},
+        {"Gender": "Male", "HeightCm": 161, "WeightKg": 85},
+        {"Gender": "Male", "HeightCm": 180, "WeightKg": 77},
+        {"Gender": "Female", "HeightCm": 166, "WeightKg": 62},
+        {"Gender": "Female", "HeightCm": 150, "WeightKg": 70},
+        {"Gender": "Female", "HeightCm": 167, "WeightKg": 82}]) == [{'Gender': 'Male', 'HeightCm': 171, 'WeightKg': 96, 'BMI': 32.83061454806607, 'BMICatergory': 'Moderately Obese', 'Health Risk': 'Medium risk'}, {'Gender': 'Male', 'HeightCm': 161, 'WeightKg': 85, 'BMI': 32.79194475521777, 'BMICatergory': 'Moderately Obese', 'Health Risk': 'Medium risk'}, {'Gender': 'Male', 'HeightCm': 180, 'WeightKg': 77, 'BMI': 23.76543209876543, 'BMICatergory': 'Normal Weight', 'Health Risk': 'Low risk'}, {'Gender': 'Female', 'HeightCm': 166, 'WeightKg': 62, 'BMI': 22.49963710262738, 'BMICatergory': 'Normal Weight', 'Health Risk': 'Low risk'}, {'Gender': 'Female', 'HeightCm': 150, 'WeightKg': 70, 'BMI': 31.11111111111111, 'BMICatergory': 'Moderately Obese', 'Health Risk': 'Medium risk'}, {'Gender': 'Female', 'HeightCm': 167, 'WeightKg': 82, 'BMI': 29.402273297715947, 'BMICatergory': 'Overweight', 'Health Risk': 'Enhanced risk'}]
+
 
 def get_bmi(d):
     l = len(d)
@@ -31,13 +39,5 @@ def get_bmi(d):
             d[i]["Health Risk"] = "Very high risk"
     print(d)
     return d
-
-def test_get_bmi():
-    assert get_bmi([{"Gender": "Male", "HeightCm": 171, "WeightKg": 96},
-        {"Gender": "Male", "HeightCm": 161, "WeightKg": 85},
-        {"Gender": "Male", "HeightCm": 180, "WeightKg": 77},
-        {"Gender": "Female", "HeightCm": 166, "WeightKg": 62},
-        {"Gender": "Female", "HeightCm": 150, "WeightKg": 70},
-        {"Gender": "Female", "HeightCm": 167, "WeightKg": 82}]) == [{'Gender': 'Male', 'HeightCm': 171, 'WeightKg': 96, 'BMI': 32.83061454806607, 'BMICatergory': 'Moderately Obese', 'Health Risk': 'Medium risk'}, {'Gender': 'Male', 'HeightCm': 161, 'WeightKg': 85, 'BMI': 32.79194475521777, 'BMICatergory': 'Moderately Obese', 'Health Risk': 'Medium risk'}, {'Gender': 'Male', 'HeightCm': 180, 'WeightKg': 77, 'BMI': 23.76543209876543, 'BMICatergory': 'Normal Weight', 'Health Risk': 'Low risk'}, {'Gender': 'Female', 'HeightCm': 166, 'WeightKg': 62, 'BMI': 22.49963710262738, 'BMICatergory': 'Normal Weight', 'Health Risk': 'Low risk'}, {'Gender': 'Female', 'HeightCm': 150, 'WeightKg': 70, 'BMI': 31.11111111111111, 'BMICatergory': 'Moderately Obese', 'Health Risk': 'Medium risk'}, {'Gender': 'Female', 'HeightCm': 167, 'WeightKg': 82, 'BMI': 29.402273297715947, 'BMICatergory': 'Overweight', 'Health Risk': 'Enhanced risk'}]
 
 get_bmi(data)
