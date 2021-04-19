4b27b48815cb8fcee18732a74635220043d6a4cf
PranavByali <63644120+PranavByali@users.noreply.github.com>
2021-04-19 17:04:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,9 +29,8 @@ def get_bmi(d):
         if bmi >= 40:
             d[i]["BMICatergory"] = "Very seriously obese"
             d[i]["Health Risk"] = "Very high risk"
-     print(d)
-     return d   
-        
+    print(d)
+    return d
 
 def test_get_bmi():
     assert get_bmi([{"Gender": "Male", "HeightCm": 171, "WeightKg": 96},
